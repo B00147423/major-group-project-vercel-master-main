368501768a1d654863daddf46096f4736b481404
B00147423 <b00147423@mytudublin.ie>
2024-04-29 17:33:25 +0100

    update

--- major-group-project-vercel-master-main/major-group-project-vercel-master/src/app/createPost/page.js
+++ major-group-project-vercel-master-main/major-group-project-vercel-master/src/app/createPost/page.js
@@ -43,9 +43,7 @@ async function runDBCallAsync(url, formData){
 
 const createPost = () => {
     const [username, setUsername] = useState('');
-    if (typeof localStorage !== "undefined"){
       const moduleId = localStorage.getItem('currentModuleId');
-    }
     const router = useRouter(); // Using useRouter for navigation
 
     
