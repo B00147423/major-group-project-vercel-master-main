e8b1cca8ad190cb2211e8f3cfc8dc23faa3835a4
B00147423 <b00147423@mytudublin.ie>
2024-05-01 15:32:15 +0100

    update

--- major-group-project-vercel-master-main/major-group-project-vercel-master/src/app/modules/[moduleId]/page.js
+++ major-group-project-vercel-master-main/major-group-project-vercel-master/src/app/modules/[moduleId]/page.js
@@ -4,7 +4,7 @@ import { useRouter } from 'next/navigation';
 import { Button, Box, TextField } from "@mui/material";
 import Layout from '../../Components/Layout';
 import '../../css/modulePage.css';
-import Comment from '../../Comment/Comment';
+import Comment from '../../Comment';
 
 const ModulePage = () => {
   const [moduleInfo, setModuleInfo] = useState({});
