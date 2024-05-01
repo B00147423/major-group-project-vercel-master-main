64e30bc3d07aa2537c04f36dc16b3bdc20fead7b
B00147423 <b00147423@mytudublin.ie>
2024-05-01 16:38:31 +0100

    update

--- major-group-project-vercel-master-main/major-group-project-vercel-master/src/app/posts/[postId]/page.js
+++ major-group-project-vercel-master-main/major-group-project-vercel-master/src/app/posts/[postId]/page.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { Button, Box, TextField, Typography } from "@mui/material";
 import Layout from '../../Components/Layout';
 import '../../css/Comment.module.css';
