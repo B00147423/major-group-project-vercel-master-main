e5b26994b4fd9ff78e44858fae9574561e551197
B00147423 <b00147423@mytudublin.ie>
2024-05-01 15:37:40 +0100

    update

--- major-group-project-vercel-master-main/major-group-project-vercel-master/src/app/Comment/page.js
+++ major-group-project-vercel-master-main/major-group-project-vercel-master/src/app/Comment/page.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, { useState } from 'react';
 import { Button, TextField, Typography } from '@mui/material';
 import styles from '../css/Comment.module.css';
